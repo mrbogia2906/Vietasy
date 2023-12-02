656972c3996d9ad4a1612d1a35e8db0542de621c
mrbogia2906 <nvk2906@gmail.com>
2023-12-02 19:10:33 +0700

    Update profile_screen.dart

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:vietasy/sign_in.dart';
-import 'package:vietasy/sign_up.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -18,23 +17,86 @@ class _ProfileScreenState extends State<ProfileScreen> {
         title: Text("Profile"),
       ),
       body: Center(
-        child: Column(
-          children: [
-            CircleAvatar(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: CircleAvatar(
               radius: 100,
               backgroundImage: AssetImage('assets/images/7cho.jpeg'),
             ),
-            Text("B.O.A.T"),
-            ElevatedButton(onPressed: (){
+          ),
+          Text(
+            "B.O.A.T",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 26),
+          ),
+          SizedBox(height: 10,),
+          Card(
+            margin: const EdgeInsets.only(left: 25, right: 25, bottom: 10),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            child: const ListTile(
+              leading: Icon(
+                Icons.edit,
+                color: Colors.black54,
+              ),
+              title: Text(
+                'Edit Profile',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios_outlined,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.only(left: 25, right: 25, bottom: 10),
+            shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            child: const ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.black54,
+              ),
+              title: Text(
+                'Settings',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios_outlined,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: (){
               Navigator.pushAndRemoveUntil(
                 context,
                 MaterialPageRoute(builder: (context) => SignIn()),
                     (route) => false,
               );
-            }, child: Text('Log out')
+            },
+            child: Card(
+              margin: const EdgeInsets.only(left: 25, right: 25, bottom: 10),
+              shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+              child: const ListTile(
+                leading: Icon(
+                  Icons.logout,
+                  color: Colors.black54,
+                ),
+                title: Text(
+                  'Log out',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios_outlined,
+                  color: Colors.black54,
+                ),
+              ),
             ),
-          ]
-        ),
+          ),
+        ]),
       ),
     );
   }
